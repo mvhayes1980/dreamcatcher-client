f87a0c37b2b0e165e71945f5f27bc17ce343dec0
Michael <hayesmv@yahoo.com>
2020-05-27 14:04:02 -0400

    post token solution

---
+++ src/components/site/auth/Auth.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type AcceptedProps = {
+    updateToken: (newToken: string) => void
+}
+
+export default class Auth extends React.Component <AcceptedProps, {}> {
+    constructor(props: AcceptedProps){
+        super(props)
+        this.state = {
+        }
+        
+    }
+
+    render(){
+        return(
+            <div>
+                    <button onClick={() => this.props.updateToken("gregarious chives")}>click to change sessiont toknen</button>
+            </div>
+        )
+    }
+}
