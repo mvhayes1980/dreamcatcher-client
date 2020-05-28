1e56a6c05e04ba7cad20272f8e78b4ff2542aa75
Jerad Lollar <jeradlollar@gmail.com>
2020-05-28 14:44:26 -0400

    jerad's branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Home from './components/site/Home';
-import Auth from './components/site/auth/Auth';
+import Auth from './components/auth/Auth';
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './App.css';
