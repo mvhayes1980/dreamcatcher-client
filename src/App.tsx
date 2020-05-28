ee9868bba277b51245de250735c98d9f593340a3
Jerad Lollar <jeradlollar@gmail.com>
2020-05-28 14:05:42 -0400

    jerad's branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
 import * as React from 'react';
 import Home from './components/site/home';
+import Auth from './components/site/auth/Auth';
 import './App.css';
 
-class App extends React.Component {
+type AppState = {
+  sessionToken: string | null,
+  isAdmin: boolean,
+  nsfwOk: boolean
+}
+
+class App extends React.Component <{}, AppState> {
+  constructor(props: string){
+    super(props)
+    this.state = {
+      sessionToken: "",
+      isAdmin: false,
+      nsfwOk: false
+    }
+  }
+
+  componentWillMount(){
+    if(localStorage.getItem('dreamSessionToken')) {
+      this.setState({sessionToken: localStorage.getItem('dreamSessionToken')});
+      console.log("sessionToken:", localStorage.getItem('dreamSessionToken'));
+    }
+  }
+
+  updateToken(newToken: string){
+    localStorage.setItem('dreamSessionToken', newToken);
+    this.setState({sessionToken: newToken});
+    console.log("sessionToken changed to:", newToken)
+  }
+
+  clearToken(){
+    localStorage.removeItem('dreamSessionToken');
+    this.setState({sessionToken: ""})
+  }
+
+
+
   public render () {
     return (
       <div className="App">
           <div className='main'>
-
+            {this.state.sessionToken != "" ? "component here:" : <Auth updateToken={(newToken)=>{this.updateToken(newToken)}}/>}
+            <button onClick={() => {this.clearToken()}}>Clear</button>
           </div>
       </div>
     );
   }
-  
 }
 
 export default App;
