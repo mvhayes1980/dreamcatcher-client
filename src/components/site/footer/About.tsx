f9007455b42aa68ea5f98d0111d5c5b2a1161d9a
Michael <hayesmv@yahoo.com>
2020-06-03 18:54:27 -0400

    fixed profile button margin

---
+++ src/components/site/footer/About.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type AcceptedProps = {
+    sessionToken: string
+}
+
+export default class About extends React.Component <AcceptedProps> {
+    constructor(props: AcceptedProps) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+
+    render(){
+        return(
+            <div>
+
+
+                
+            </div>
+        )
+    }
+}
