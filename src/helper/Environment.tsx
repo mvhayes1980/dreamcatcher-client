8b100bdea6642c4deb2661a43fb416322c3a4bb4
Michael V. Hayes <58855390+mvhayes1980@users.noreply.github.com>
2021-02-27 21:05:09 -0500

    Update Environment.tsx

--- src/helper/Environment.tsx
+++ src/helper/Environment.tsx
@@ -1,11 +1,11 @@
-let APIURL = 'https://dreamcatcher-server.herokuapp.com';
+let APIURL = 'https://dreamalish2.herokuapp.com';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3002';
         break;
-    case 'dreamalish.herokuapp.com':
-        APIURL = 'https://dreamcatcher-server.herokuapp.com';
+    case 'dreamalish-client.herokuapp.com':
+        APIURL = 'https://dreamalish2.herokuapp.com';
         
 }
 
